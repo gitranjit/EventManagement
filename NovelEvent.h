d73309de2d361797f11a69fb959317ab0f0d6bd2
gitranjit <92791720+gitranjit@users.noreply.github.com>
2024-02-28 11:08:53 +0530

    Update NovelEvent.h
    Updated structure

--- NovelEvent.h
+++ NovelEvent.h
@@ -8,131 +8,148 @@ using namespace std;
 
 class Date {
 protected:
-  int day;
-  int month;
-  int year;
+	int day;
+	int month;
+	int year;
 
 public:
-  friend class ManagementSystem;
-  Date();
-  Date(int day, int month, int year);
-  void set_date();
-  void setNew_date();
-  int get_day();
-  int get_month();
-  int get_year();
+	friend class ManagementSystem;
+	Date();
+	Date(int day, int month, int year);
+	void set_date();
+	void setNew_date();
+	int get_day();
+	int get_month();
+	int get_year();
 };
 
 class Guests {
-  string name;
-  string mobile_number;
+	string name;
+	string mobile_number;
 
 public:
-  Guests();
-  Guests(string name, string mobile_number);
-  void setGuests();
-  string getGuestsName();
-  string getGuestsMob();
+	Guests();
+	Guests(string name, string mobile_number);
+	void setGuests();
+	string getGuestsName();
+	string getGuestsMob();
 };
 
 class Event {
 private:
-  static int eventCount;
-  int breakfast_count = 0;
-  int tea_count = 0;
-  int meal_count = 0;
-  int status = 0; // -1 for cancelled , 0 for pending , 1 for complete
+	static int eventCount;
+	int breakfast_count = 0;
+	int tea_count = 0;
+	int meal_count = 0;
+	int status = 0; // -1 for cancelled , 0 for pending , 1 for complete
 
-  int hotelRooms = 0;
+	int hotelRooms = 0;
 
 protected:
-  int breakfastLim = 0;
-  int mealLim = 0;
-  int teaLim = 0;
-  string nameOfEvent;
-  float cost;
-  float remained_cost = 0.0;
-  int noOfGuests;
-  float advance;
-  float advanceCost;
-  Date eventDate; // object of date class
+	int breakfastLim = 0;
+	int mealLim = 0;
+	int teaLim = 0;
+	string nameOfEvent;
+	float cost;
+	float remained_cost = 0.0;
+	int noOfGuests;
+	float advance;
+	float advanceCost;
+	Date eventDate; // object of date class
 
 public:
-  Event();
-  int eventId;
+	Event();
 
-  friend class ManagementSystem;
+	int get_breakfastLim();
+	int get_mealLim();
+	int get_teaLim();
+	int get_breakfast_count();
+	int get_tea_count();
+	int get_meal_count();
+	void set_remained_cost(int cost);
+	int get_remained_cost();
 
-  void setEventId();
+	int eventId;
 
-  void setEventName();
+	friend class ManagementSystem;
 
-  void setNewEventName();
+	void setStatus(int inputStatus);
 
-  string getEventName();
+	int getStatus();
 
-  void setNoOfGuests();
+	int getEventId();
 
-  int getNoOfGuests();
+	void setEventId();
 
-  void add_guest();
+	void setEventName();
 
-  void setFood();
+	void setNewEventName();
 
-  void calculate_cost();
+	string getEventName();
 
-  void generate_invitation();
+	void setNoOfGuests();
 
-  void advance_payment();
+	int getNoOfGuests();
 
-  void cancellationCharges(int tempStatus);
+	void add_guest();
 
-  float getCost();
+	void setFood();
 
-  void setEventDate();
+	void generate_invitation();
 
-  void setNewEventDate();
+	void advance_payment();
 
-  void bookHotel();
+	void cancellationCharges(int tempStatus);
 
-  void pending_payment();
+	float getCost();
 
-  void final_payment();
+	void setEventDate();
+
+	void setNewEventDate();
+
+	void bookHotel();
+
+	void pending_payment();
 };
 
 class ManagementSystem {
 public:
-  static vector<Event *> eventList;
-  vector<int> cancelledId;
-  void cancelledCharges(int Id2);
-  void generateInvitation(int ID);
-  void addGuestForEvent(int Id1);
-  void storeEvent(Event *event);
-  void storeUpdatedEvent(Event *event, int eventUpdateId, int flag);
-  void deleteCancelledEvent();
-  void showEventsInMonth(int month);
-  void viewAllEvents(Event *event);
-  void menu();
+	static vector<Event*> eventList;
+	vector<int> cancelledId;
+	void calculate_cost(int id);
+	void adminMenu();
+	void userMenu();
+	void cancelledCharges(int Id2);
+	void generateInvitation(int ID);
+	void addGuestForEvent(int Id1);
+	void storeEvent(Event* event);
+	void storeUpdatedEvent(Event* event, int eventUpdateId, int flag);
+	void deleteCancelledEvent();
+	void showEventsInMonth(int month);
+	void final_payment();
+	void viewAllEvents();
+	void eventStatusMonth();
+	void menu();
 };
 
 class shortEvent : public Event { // 4 hrs
 public:
-  shortEvent();
+	shortEvent();
 };
 
 class mediumEvent : public Event { // 8 hrs
 public:
-  mediumEvent();
+	mediumEvent();
 };
 
 class longEvent : public Event { // 12 hrs
 public:
-  longEvent();
+	longEvent();
 };
 
 class extraLongEvent : public Event { // 24 hrs
 public:
-  extraLongEvent();
+	extraLongEvent();
 };
 
 #endif // NOVELEVENT_H
